6603cc23ef68d8c864ba5591eb85c49aabf6a211
unknown <stefanikrautstrunk@gmail.com>
2018-06-19 21:02:51 -0700

    icon added and JS code for no button dead page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # project1
 This is the repo for project 1
+
+Icon on landing page is from <a href="https://icons8.com">Icon pack by Icons8</a>
--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,5 +1,5 @@
 // function to get from the landing page to results page
-var yesButton = $('#id').on('click', function() {
+var yesButton = $('#yes').on('click', function() {
     console.log("yesButton");
     $(this).parent().hide();
     $('.container').show();
@@ -8,5 +8,18 @@ var yesButton = $('#id').on('click', function() {
 
 // function for displaying the results page
 var resultsPage = function() {
-    
+
+}
+
+// function to bring user to a dead page if no is clicked
+var noButton = $('#no').on('click', function () {
+    console.log("noButton");
+    $(this).parent().hide();
+    $('.container').show();
+    deadPage();
+});
+
+// function for displaying the dead page
+var deadPage = function() {
+
 }
