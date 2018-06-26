efb4c8eb081651dcebe57757bc254c4c752756ea
Your Name <you@example.com>
2018-06-25 17:55:09 -0700

    update to test

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-
+//Firebase Coding for logging the zipcodes
     var config = {
         apiKey: "AIzaSyAUo4yqSzNjCfAdx9tGPN3HtgnAmb4zDbk",
         authDomain: "brewfinder-da560.firebaseapp.com",
@@ -32,7 +32,25 @@ $(document).ready(function () {
     })
 
      
-
+    $("#submit-button").on("click", function () {
+        var cityZip = $("#zipCode");
+        console.log(cityZip);
+        var Zip = cityZip [0].value;
+        var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=brewery+"+ 
+        Zip + "+food&sensor=false&location=40.67,-73.94&radius=100&key=AIzaSyAxmDmflbG6AVvQ_RoEbLmt6AWnSoTVMvA";
+        console.log(queryURL);
+        console.log(Zip);
+        $.ajax({
+          url: queryURL,
+          method: "GET",
+          // data: cityZip,
+          dataType: 'jsonp'
+          // - using this erases failure to load.
+          // cache: false,
+        }).then(function(response) {
+          var results = response.data;
+        })
+        });
    
 
 
